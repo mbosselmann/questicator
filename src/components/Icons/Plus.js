2c9ce861c26054206d16741704cf9d13a29151fc
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-28 12:39:41 +0100

    Add plus icon

---
+++ src/components/Icons/Plus.js
@@ -0,0 +1,30 @@
+export default function Plus() {
+  return (
+    <svg
+      width="36"
+      height="35"
+      viewBox="0 0 36 35"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <line
+        x1="17.5"
+        y1="2.5"
+        x2="17.5"
+        y2="32.5"
+        stroke="white"
+        strokeWidth="5"
+        strokeLinecap="round"
+      />
+      <line
+        x1="3"
+        y1="17.8583"
+        x2="33"
+        y2="17.8583"
+        stroke="white"
+        strokeWidth="5"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+}
