a4c2e52d030e8f1e23971b22f1db551ca06b1594
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-28 10:15:55 +0100

    Add data

---
+++ db.js
@@ -0,0 +1,54 @@
+export const data = [
+  {
+    id: "1",
+    title: "Learn how to be the greatest wizward in the world!",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio iusto accusamus mollitia asperiores praesentium officia, quas aliquid quia corrupti, in excepturi velit magni illum alias eveniet odit placeat maiores voluptates.",
+    labels: [
+      { id: "1", name: "practice" },
+      { id: "2", name: "high-priority" },
+    ],
+    isDone: true,
+  },
+  {
+    id: "2",
+    title: "Drink a beer with the magic pig!",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio iusto accusamus mollitia asperiores praesentium officia, quas aliquid quia corrupti, in excepturi velit magni illum alias eveniet odit placeat maiores voluptates.",
+    labels: [
+      { id: "1", name: "discovery" },
+      { id: "2", name: "low-priority" },
+    ],
+    isDone: true,
+  },
+  {
+    id: "3",
+    title: "Clean your house!",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio iusto accusamus mollitia asperiores praesentium officia, quas aliquid quia corrupti, in excepturi velit magni illum alias eveniet odit placeat maiores voluptates.",
+    labels: [
+      { id: "1", name: "protect" },
+      { id: "2", name: "high-priority" },
+    ],
+    isDone: false,
+  },
+  {
+    id: "4",
+    title: "Buy some new armor!",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio iusto accusamus mollitia asperiores praesentium officia, quas aliquid quia corrupti, in excepturi velit magni illum alias eveniet odit placeat maiores voluptates.",
+    labels: [
+      { id: "1", name: "protect" },
+      { id: "2", name: "low-priority" },
+    ],
+    isDone: true,
+  },
+  {
+    id: "5",
+    title: "Build up your strength!",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio iusto accusamus mollitia asperiores praesentium officia, quas aliquid quia corrupti, in excepturi velit magni illum alias eveniet odit placeat maiores voluptates.",
+    labels: [{ id: "1", name: "practice" }],
+    isDone: false,
+  },
+];
