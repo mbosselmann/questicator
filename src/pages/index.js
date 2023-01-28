78535a64aa8309e4c5b8f0550ddc800a5b620d06
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-28 10:19:23 +0100

    Update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <h1>Questicator</h1>;
+import QuestList from "components/QuestList.js";
+
+export default function Home({ quests, updateQuestStatus }) {
+  const unsolvedQuests = quests.filter(({ isDone }) => !isDone);
+
+  if (unsolvedQuests.length === 0) {
+    return <p>No open quests yet.</p>;
+  }
+
+  return (
+    <QuestList quests={unsolvedQuests} updateQuestStatus={updateQuestStatus} />
+  );
 }
