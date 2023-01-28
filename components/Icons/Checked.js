93e3407efcf96002d31ee47735f254776b2347f6
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-28 11:58:45 +0100

    Add input icons

---
+++ components/Icons/Checked.js
@@ -0,0 +1,38 @@
+export default function Checked() {
+  return (
+    <svg
+      width="30"
+      height="30"
+      viewBox="0 0 60 60"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="5"
+        y="5"
+        width="50"
+        height="50"
+        rx="10"
+        fill="#6E85B2"
+        stroke="white"
+        strokeWidth="5"
+      />
+      <path
+        d="M17.0001 31.4194L27.1276 41.6334"
+        stroke="white"
+        strokeWidth="5"
+        strokeLinecap="round"
+      />
+      <line
+        x1="2.5"
+        y1="-2.5"
+        x2="27.9962"
+        y2="-2.5"
+        transform="matrix(-0.572685 0.819776 -0.77035 -0.637621 42 17)"
+        stroke="white"
+        strokeWidth="5"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+}
---
+++ components/Icons/NotChecked.js
@@ -0,0 +1,22 @@
+export default function NotChecked() {
+  return (
+    <svg
+      width="30"
+      height="30"
+      viewBox="0 0 60 60"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="5"
+        y="5"
+        width="50"
+        height="50"
+        rx="10"
+        fill="#261C2C"
+        stroke="white"
+        strokeWidth="5"
+      />
+    </svg>
+  );
+}
