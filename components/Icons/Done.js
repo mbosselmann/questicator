7dfcc2ecde4c7bbdfae1e8449f256f55cf13cfc3
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-28 10:16:59 +0100

    Add icons as components

---
+++ components/Icons/Done.js
@@ -0,0 +1,35 @@
+export default function Done() {
+  return (
+    <svg
+      width="35"
+      height="35"
+      viewBox="0 0 250 250"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        cx="125"
+        cy="125"
+        r="117.5"
+        fill="#5C527F"
+        stroke="white"
+        strokeWidth="15"
+      />
+      <path
+        d="M63.0002 133.176L112.827 179.343"
+        stroke="white"
+        strokeWidth="20"
+        strokeLinecap="round"
+      />
+      <line
+        x1="187.907"
+        y1="82.013"
+        x2="114.087"
+        y2="179.093"
+        stroke="white"
+        strokeWidth="20"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+}
