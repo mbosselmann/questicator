fc523b0b9e14049510be7cd1c3a376691f1d10d5
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-28 10:19:10 +0100

    Update app file

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,37 @@
+import { data } from "db.js";
+import { useImmerLocalStorageState } from "lib/hook/useImmerLocalStorageState.js";
+import Layout from "components/Layout.js";
+import GlobalStyle from "../styles.js";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [quests, setQuests] = useImmerLocalStorageState("quests", {
+    defaultValue: [],
+  });
+
+  function updateQuestStatus(questId) {
+    if (quests) {
+      setQuests(
+        quests.map((quest) =>
+          quest.id === questId ? { ...quest, isDone: !quest.isDone } : quest
+        )
+      );
+    } else {
+      setQuests(
+        data.map((quest) =>
+          quest.id === questId ? { ...quest, isDone: !quest.isDone } : quest
+        )
+      );
+    }
+  }
+
+  return (
+    <Layout>
+      <GlobalStyle />
+      <Component
+        {...pageProps}
+        quests={quests}
+        updateQuestStatus={updateQuestStatus}
+      />
+    </Layout>
+  );
 }
