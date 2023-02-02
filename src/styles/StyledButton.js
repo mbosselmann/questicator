477a8ee5215fc01bded69ded21f3a12c895b4fb9
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-02-02 23:28:16 +0100

    Create styled button component

---
+++ src/styles/StyledButton.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  background-color: var(--highlighted);
+  border: none;
+  padding: 0.5rem;
+  border-radius: 0.3rem;
+  color: white;
+  font-weight: bold;
+  font-family: inherit;
+  font-size: 1rem;
+`;
