11a9c61ecdfeca2220ee9955c3a74a2cf8b353e2
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-02-02 23:26:24 +0100

    Update example data

--- db.js
+++ db.js
@@ -1,5 +1,35 @@
 export const data = [
   {
+    id: "d3fa0144-cbdd-4845-b215-e9c6c155e68e",
+    title: "Find the golden wolf",
+    description: "Maybe there is also a treasure waiting for me?",
+    labels: [
+      { id: "05bec04c-b139-4424-a253-ee554db94bdf", name: "discovery" },
+      { id: "eac02276-6a5b-40bc-9a38-3861ac379606", name: "low-priority" },
+    ],
+    isDone: false,
+  },
+  {
+    id: "aa32416f-8e03-4325-bc8b-399f7a80512a",
+    title: "Find the fairy floof!",
+    description: "This one could be a tricky adventure.",
+    labels: [
+      { id: "8c46b909-3d6d-40e5-8fa9-adfefcadded1", name: "protect" },
+      { id: "a8062775-70a5-4888-b7ae-c81f425ffede", name: "low-priority" },
+    ],
+    isDone: false,
+  },
+  {
+    id: "784fdeed-87cf-486c-b5be-744559b0585f",
+    title: "Build a castle!",
+    description: "This is a very interesting quest!",
+    labels: [
+      { id: "bf0548eb-c1ce-4281-bb97-054b4327fa23", name: "discovery" },
+      { id: "a8062775-70a5-4888-b7ae-c81f425ffede", name: "high-priority" },
+    ],
+    isDone: false,
+  },
+  {
     id: "1",
     title: "Learn how to be the greatest wizward in the world!",
     description:
@@ -8,18 +38,15 @@ export const data = [
       { id: "1", name: "practice" },
       { id: "2", name: "high-priority" },
     ],
-    isDone: true,
+    isDone: false,
   },
   {
     id: "2",
-    title: "Drink a beer with the magic pig!",
+    title: "Drink a beer with the magic pig and fairy cat!",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio iusto accusamus mollitia asperiores praesentium officia, quas aliquid quia corrupti, in excepturi velit magni illum alias eveniet odit placeat maiores voluptates.",
-    labels: [
-      { id: "1", name: "discovery" },
-      { id: "2", name: "low-priority" },
-    ],
-    isDone: true,
+      "This will be an amazing opportunity to sell your handmade beer.",
+    labels: [{ id: "8ea7df4c-aeea-484d-abd2-1252ed02406b", name: "practice" }],
+    isDone: false,
   },
   {
     id: "3",
@@ -41,14 +68,14 @@ export const data = [
       { id: "1", name: "protect" },
       { id: "2", name: "low-priority" },
     ],
-    isDone: true,
+    isDone: false,
   },
   {
     id: "5",
     title: "Build up your strength!",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio iusto accusamus mollitia asperiores praesentium officia, quas aliquid quia corrupti, in excepturi velit magni illum alias eveniet odit placeat maiores voluptates.",
-    labels: [{ id: "1", name: "practice" }],
+    labels: [{ id: "9c7df32b-34a5-4b06-92fb-1bee424a0c61", name: "protect" }],
     isDone: false,
   },
 ];
