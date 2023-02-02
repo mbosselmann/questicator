6a48598d4e6114d08ec71db64523379ec9bc40bb
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-02-02 23:31:07 +0100

    Rename and update input component

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -3,9 +3,22 @@ import styled from "styled-components";
 const Wrapper = styled.p`
   display: grid;
   gap: 0.3rem;
+  margin: 0;
 `;
 
-export default function Input({
+const Input = styled.input`
+  padding: 0.4rem;
+  border-radius: 0.3rem;
+  border: none;
+  font-family: inherit;
+  font-size: 1rem;
+
+  &:focus {
+    outline: 3px solid var(--highlighted);
+  }
+`;
+
+export default function TextInput({
   id,
   name,
   labelText,
@@ -16,7 +29,7 @@ export default function Input({
   return (
     <Wrapper>
       <label htmlFor={id}>{labelText}</label>
-      <input
+      <Input
         type={type}
         id={id}
         name={name}
