9d9283a244a2d8932bb4b8aeb4cf8ee6a8258596
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-02-03 12:41:59 +0100

    Update navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,23 @@
 import { useRouter } from "next/router";
 import Link from "next/link.js";
-import styled, { css } from "styled-components";
-import Done from "./Icons/Done.js";
-import Scroll from "./Icons/Scroll.js";
-import { ScreenReaderOnly } from "./ScreenReaderOnly.js";
-import Plus from "./Icons/Plus.js";
+import styled from "styled-components";
+import Done from "../assets/Icons/Done.js";
+import Scroll from "../assets/Icons/Scroll.js";
+import { ScreenReaderOnly } from "../styles/ScreenReaderOnly.js";
+import Plus from "../assets/Icons/Plus.js";
 
 const NavBar = styled.nav`
   background-color: var(--dark-bg-color);
   display: flex;
   justify-content: space-between;
-  border-top: 5px solid #3e2c41;
 `;
 
 const StyledLink = styled(Link)`
   height: 100%;
   flex-grow: 1;
   color: var(--text-color);
-  background-color: ${({ active }) => (active ? "#3E2C41" : "transparent")};
+  background-color: ${({ active }) =>
+    active ? "var(--light-bg-color)" : "transparent"};
   text-decoration: none;
   display: grid;
   place-items: center;
