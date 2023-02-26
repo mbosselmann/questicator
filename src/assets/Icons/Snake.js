f33cff6c5182149742eea3c8b817263001643fe7
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-02-26 02:12:18 +0100

    Create snake icon

---
+++ src/assets/Icons/Snake.js
@@ -0,0 +1,24 @@
+export default function Snake() {
+  return (
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 293 310"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M41.9959 77.0288C41.9959 77.0288 64.8543 47.4236 90.4958 42.5288C114.003 38.0415 139.496 42.5288 145.996 66.0287C163.196 128.21 55.6206 156.197 60.496 220.529C61.8487 238.379 67 246 79.0005 255C150.976 308.979 161.585 181.432 204.5 187.5C239.499 192.448 193.789 258.101 229 255C253.704 252.824 279.501 216.5 279.501 216.5"
+        stroke="white"
+        strokeWidth="35"
+        strokeLinecap="round"
+      />
+      <path
+        d="M44 76.9999L74 49.9999"
+        stroke="white"
+        strokeWidth="80"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+}
