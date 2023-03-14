a26e4cfbe81f12fe1dff9f9d3ad70f3b605dbd78
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-03-14 21:01:50 +0100

    Update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,23 @@
 import styled, { keyframes } from "styled-components";
 
 import Questicator from "@/assets/Icons/Questicator.js";
-import QuestList from "@/components/QuestList.js";
-import { StyledButton } from "@/styles/StyledButton.js";
-import { useState } from "react";
+import { StyledLink } from "@/styles/StyledLink.js";
 
-export default function Home({ unsolvedQuests, updateQuestStatus }) {
-  const [chosenQuests, setChosenQuests] = useState([]);
-
+export default function Home() {
   return (
     <Wrapper>
-      <Questicator width="250px" height="250px" />
-      <Slide>
+      <>
+        <Slide>
+          <Questicator width="250px" height="250px" />
+        </Slide>
         <h2>Oh no! The Questicator has appeared!</h2>
         <p>
           You need to defeat this powerful creature. Solve three quests from the
           list above to win against the Questicator. Which of the following
           quests do you want to choose?
         </p>
-        <StyledButton type="button">Choose quests</StyledButton>
-      </Slide>
-
-      <h2>Available Quests</h2>
-      <p>Number of choosen quests: {chosenQuests.length}</p>
-      <QuestList
-        quests={unsolvedQuests}
-        updateQuestStatus={updateQuestStatus}
-      />
+        <StyledLink href="/availablequests">Choose quests</StyledLink>
+      </>
     </Wrapper>
   );
 }
